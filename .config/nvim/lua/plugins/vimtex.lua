7116dc077239798becf78063e23a4e101b9ebbe3
Tom <tom.lehmann@tu-darmstadt.de>
2026-02-06 10:46:47 +0100

    minor changes

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -5,7 +5,9 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = 'general'
-    vim.g.vimtex_view_general_viewer = vim.fn.expand("/home/tom/.local/bin/sumatrapdf.sh")
+		-- chose viewer depending on installed viewer or operating system
+		vim.g.vimtex_view_general_viewer = zathura
+    -- vim.g.vimtex_view_general_viewer = vim.fn.expand("/home/tom/.local/bin/sumatrapdf.sh")
     vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
 
   end
